f2a1dcdc0ecae6ef6d233740e3c359f8bc1884e8
Rafael Borges <rafaelbertelli@gmail.com>
2019-03-27 14:46:02 -0300

    Convert to ES6 destructuring arr return

--- src/map/map-recursive.js
+++ src/map/map-recursive.js
@@ -6,7 +6,10 @@ const map = (arr = [], func = item => item) => {
     if (arrayInternal.length === 0) return []
     const [head, ...tail] = arrayInternal
     
-    return [func(head, counter, arr)].concat(mapInternal(tail, counter + 1))
+    return [
+      func(head, counter, arr),
+      ...mapInternal(tail, counter + 1)
+    ]
   })(arr, 0)
 }
 
